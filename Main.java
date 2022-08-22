e38691b500f6683e25ffcb3ffbf6b7bd8fdd914f
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2022-08-22 08:52:49 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+package com.challenge;
+
+public class Main {
+    public static void main(String[] args) {
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        double price = hamburger.itemizedHamburger();
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.addHamburgerAddition3("Cheese", 1.13);
+        System.out.println("Total Burger price is " + hamburger.itemizedHamburger());
+
+        HealthyBurger healthyBurger = new HealthyBurger("Bacon", 5.67);
+        healthyBurger.addHamburgerAddition1("Egg", 5.43);
+        healthyBurger.addHealthAddition1("Lentils", 3.41);
+        System.out.println("Total Healthy Burger price is " + healthyBurger.itemizedHamburger());
+
+        DeluxeBurger db = new DeluxeBurger();
+        db.addHamburgerAddition3("Should not fo this", 50.53);
+        db.itemizedHamburger();
+    }
+}
